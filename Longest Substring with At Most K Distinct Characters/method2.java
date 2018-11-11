a4890c13dac490eff6760fab2cd43397be9905d6
Andrew Guan <31747328+AndrewGuan@users.noreply.github.com>
2018-11-11 03:33:41 -0600

    Create method2.java

---
+++ Longest Substring with At Most K Distinct Characters/method2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        int[] chrFreq = new int[256]; // including ASCII and extended ASCII
+        int l = 0;
+        int max = 0;
+        int num = 0;
+        if(k ==0)  return 0;
+        for(int i= 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            chrFreq[currentChar]++;
+            if(chrFreq[currentChar] == 1) num++;
+            if(num > k) {
+                while(chrFreq[s.charAt(l)] > 1){
+                    chrFreq[s.charAt(l)]--;
+                    l++;
+                }
+                //l++;
+                num--;
+            }
+            max = Math.max(max, i - l + 1);
+        }
+        return max;
+    }
+}
