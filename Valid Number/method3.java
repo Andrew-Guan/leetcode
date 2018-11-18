0fdf2346f7161b56cb2f32b882baf1266412c1c6
Andrew Guan <31747328+AndrewGuan@users.noreply.github.com>
2018-11-18 03:31:10 -0600

    Update method3.java

--- Valid Number/method3.java
+++ Valid Number/method3.java
@@ -1,5 +1,5 @@
 class Solution {
     public boolean isNumber(String s) {
-        return s.trim().matches("[+-]?(\\d|\\.\\d+|\\d+\\.\\d*?)\\d*(e[+-]?\\d+)?");
+        return s.trim().matches("[+-]?(\\d|\\.\\d+|\\d+\\.\\d*)\\d*(e[+-]?\\d+)?");
     }
 }
