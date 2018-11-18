402f8f550f0fdad2c83aa24e720b2f6b66bf87a7
Andrew Guan <31747328+AndrewGuan@users.noreply.github.com>
2018-11-18 03:51:02 -0600

    Create method1.java

---
+++ Valid Parentheses/method1.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s == null) return false;
+        if(s.length() == 0) return true;
+        int length = s.length();
+        Stack<Character> res = new Stack();
+        for(int i = 0; i< length; i++){
+            if(s.charAt(i) == '(') {
+                res.push(s.charAt(i));
+            }else if(s.charAt(i) == '{'){
+                res.push(s.charAt(i));
+            }else if(s.charAt(i) == '['){
+                res.push(s.charAt(i));
+            }else if(s.charAt(i) == ')'){
+                if(!res.empty()){
+                    if(res.pop() == '('){
+                        continue;
+                    } else return false;
+                } else{
+                    return false;
+                }
+            }else if(s.charAt(i) == '}'){
+                if(!res.empty()){
+                    if(res.pop() == '{'){
+                        continue;
+                    } else return false;
+                } else{
+                    return false;
+                }
+            }else{
+                if(!res.empty()){
+                    if(res.pop() == '['){
+                        continue;
+                    } else return false;
+                } else{
+                    return false;
+                }
+            }
+        }
+        if(res.empty()) return true;
+        else return false;
+    }
+}
