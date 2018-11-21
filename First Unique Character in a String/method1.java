a09e37a124cbe9b5c30bdb26217aa01152db7ec6
Andrew Guan <31747328+AndrewGuan@users.noreply.github.com>
2018-11-20 22:34:45 -0600

    Create method1.java

---
+++ First Unique Character in a String/method1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        if(s == null || s.length() == 0) return -1;
+        int length = s.length();
+        char res;
+        for(int i = 0; i< length-1;i++){
+            String subS = s.substring(i+1);
+            if(s.charAt(i)!= '0') {
+                if(subS.contains(Character.toString(s.charAt(i)))){
+                    s= s.replace(s.charAt(i), '0');
+                }else{
+                    return i;
+                }
+            }
+        }
+        if(s.charAt(length-1) == '0') return -1;
+        else return length-1;
+    }
+}
